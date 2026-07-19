680fe4ee2740f6fbd4879ab207441baf81aa360c
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-19 21:02:01 +0300

    Initial Commit

---
+++ YemeklerBasicSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YemeklerBasicSwiftUI
+//
+//  Created by Ömer Apaydın on 19.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YemeklerBasicSwiftUI/YemeklerBasicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YemeklerBasicSwiftUIApp.swift
+//  YemeklerBasicSwiftUI
+//
+//  Created by Ömer Apaydın on 19.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct YemeklerBasicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
